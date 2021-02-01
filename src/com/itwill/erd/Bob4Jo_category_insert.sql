a0eea7575bac49a73c18e30e83d93f34ae554772
cohic91 <cohic91@gmail.com>
2021-02-01 18:48:16 +0900

    ..

--- src/com/itwill/erd/Bob4Jo_category_insert.sql
+++ src/com/itwill/erd/Bob4Jo_category_insert.sql
@@ -1,5 +1,8 @@
 desc category;
 --insert
 insert into category values(10,'샌드위치');
-insert into category values(20,'랩,기타');
+insert into category values(20,'랩');
 insert into category values(30,'찹샐러드');
+insert into category values(40,'사이드메뉴');
+insert into category values(50,'쿠키,칩');
+insert into category values(60,'음료');
