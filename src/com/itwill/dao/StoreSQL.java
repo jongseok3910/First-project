d04386d8ea5ab6dba397415752960e34bd1ce807
cohic91 <cohic91@gmail.com>
2021-02-02 20:08:58 +0900

    ..

--- src/com/itwill/dao/StoreSQL.java
+++ src/com/itwill/dao/StoreSQL.java
@@ -1,5 +1,8 @@
 package com.itwill.dao;
 
 public class StoreSQL {
-
+	public static final String STORE_INSERT="";
+	public static final String STORE_SELECT_BY_NAME="";
+	public static final String STORE_UPDATE_BY_NAME="";
+	public static final String STORE_DELETE_BY_NAME="";
 }
