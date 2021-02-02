ad71e93963b198a9e7d58d71e349470946f32de1
HyeJo <zhzh2032@naver.com>
2021-02-02 22:57:51 +0900

    .

--- src/com/itwill/test/MemberInfoDaoTestMain.java
+++ src/com/itwill/test/MemberInfoDaoTestMain.java
@@ -10,21 +10,12 @@ public class MemberInfoDaoTestMain {
     public static void main(String[] args) throws Exception{
 
         MemberInfoDao memberInfoDao = new MemberInfoDao();
+        
+//        create test
+//        System.out.println("create: " + memberInfoDao.create(new MemberInfo("2021/02/028","ID6","6486-5774-1331-6456",null)));
 
-        System.out.println("create: " + memberInfoDao.create(new MemberInfo("2021/02/028","ID6","6486-5774-1331-6456",null)));
 
-
-        /*
-        MemberInfo memberInfo =  memberInfoDao.selectById(ID6");
-
-        System.out.println("selectById : " + memberInfo);
-
-        memberInfo.setMember_no("6");
-        memberInfo.setMember_id("ID6");
-        memberInfo.setCard_no("6486-5774-1331-6456");
-        memberInfo.setMember_autologin(null);
-        */
-
+   
 
     }
 }
