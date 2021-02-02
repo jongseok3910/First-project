4df93df49cca6f4c5d01689af7be013399ac30fe
cohic91 <cohic91@gmail.com>
2021-02-02 17:05:40 +0900

    vo수정

--- src/com/itwill/vo/MemberInfo.java
+++ src/com/itwill/vo/MemberInfo.java
@@ -8,8 +8,59 @@ CARD_NO                   VARCHAR2(19)
 MEMBER_AUTOLOGIN          VARCHAR2(1)  
  */
 public class MemberInfo {
-	private int member_no;
+	private String member_no;
 	private String member_id;
 	private String card_no;
 	private char member_autologin;
+	
+	public MemberInfo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public MemberInfo(String member_no, String member_id, String card_no, char member_autologin) {
+		super();
+		this.member_no = member_no;
+		this.member_id = member_id;
+		this.card_no = card_no;
+		this.member_autologin = member_autologin;
+	}
+
+	public String getMember_no() {
+		return member_no;
+	}
+
+	public void setMember_no(String member_no) {
+		this.member_no = member_no;
+	}
+
+	public String getMember_id() {
+		return member_id;
+	}
+
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+
+	public String getCard_no() {
+		return card_no;
+	}
+
+	public void setCard_no(String card_no) {
+		this.card_no = card_no;
+	}
+
+	public char getMember_autologin() {
+		return member_autologin;
+	}
+
+	public void setMember_autologin(char member_autologin) {
+		this.member_autologin = member_autologin;
+	}
+
+	@Override
+	public String toString() {
+		return "MemberInfo [member_no=" + member_no + ", member_id=" + member_id + ", card_no=" + card_no
+				+ ", member_autologin=" + member_autologin + "]";
+	}
+	
 }
