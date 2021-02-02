82658b8f44a840a86f52064657b33c1356a2a715
qkrwhddms <qkrwhddms01@gmail.com>
2021-02-02 21:58:02 +0900

    no message

--- src/com/itwill/test/MemberInfoDaoTestMain.java
+++ src/com/itwill/test/MemberInfoDaoTestMain.java
@@ -10,14 +10,16 @@ public class MemberInfoDaoTestMain {
 	public static void main(String[] args) throws Exception{
 		
 		MemberInfoDao memberInfoDao = new MemberInfoDao();
+
+		System.out.println("create: " + memberInfoDao.create(new MemberInfo("1","aaa","1234-1234",null)));
 		
-		System.out.println("create: " + memberInfoDao.create(new MemberInfo(1,"aaa","1234-1234",null)));
+		
 		/*
 		MemberInfo memberInfo =  memberInfoDao.selectById("aaa");
 		
 		System.out.println("selectById : " + memberInfo);
 		
-		memberInfo.setMember_no(1);
+		memberInfo.setMember_no("1");
 		memberInfo.setMember_id("aaa");
 		memberInfo.setCard_no("1234-1234");
 		memberInfo.setMember_autologin(null);
