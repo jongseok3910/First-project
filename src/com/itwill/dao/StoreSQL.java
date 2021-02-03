62544a20d19e56067067110861b9ac8efc6d5c89
cohic91 <cohic91@gmail.com>
2021-02-03 17:04:17 +0900

    sql,dao수정

--- src/com/itwill/dao/StoreSQL.java
+++ src/com/itwill/dao/StoreSQL.java
@@ -2,13 +2,13 @@ package com.itwill.dao;
 
 public class StoreSQL {
 	public static final String STORE_INSERT=
-			"insert into Store(store_no,store_name,store_businessTime,store_phone,store_address,store_deliveryPrice,jumun_estimatedTime,store_rating,jumun_no) values(?,?,?,?,?,?,?,?,?)"; 
+			"insert into Store values(?,?,?,?,?,?,?,?)"; 
 	public static final String STORE_SELECT_BY_NAME=
 			"select * from store where store_name=?";
 	public static final String STORE_SELECT_ALL=
 			"select * from store";
 	public static final String STORE_UPDATE_BY_NAME=
-			"update store set store_businessTime=?,store_phone=?,store_address=?,store_deliveryPrice=?,jumun_estimatedTime=?,store_rating=?,jumun_no=? where store_name=?";
+			"update store set store_businessTime=?,store_phone=?,store_address=?,store_deliveryPrice=?,jumun_estimatedTime=?,store_rating=? where store_name=?";
 	public static final String STORE_DELETE_BY_NAME=
 			"delete store where store_name=?";
 }
