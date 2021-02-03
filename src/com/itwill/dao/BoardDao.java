94947803f02c8277ac0b6b3703bb3bfdfc20252a
jongseok3910 <jongseok3910@gmail.com>
2021-02-03 11:45:19 +0900

    수정

--- src/com/itwill/dao/BoardDao.java
+++ src/com/itwill/dao/BoardDao.java
@@ -1,7 +1,6 @@
 package com.itwill.dao;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -13,71 +12,73 @@ public class BoardDao {
 	public int create(Board board) throws Exception {
 		Connection con=ConnectionFactory.getConnection();
 		PreparedStatement pstmt=con.prepareStatement(BoardSQL.BOARD_INSERT);
-		pstmt.setInt(1, board.getBoard_no());
-		pstmt.setString(2, board.getBoard_content());
-		pstmt.setInt(3, board.getBoard_rating());
-		pstmt.setInt(4, board.getStore_no());
+		pstmt.setInt(1,board.getBoard_no());
+		pstmt.setString(2,board.getBoard_content());
+		pstmt.setInt(3,board.getBoard_rating());
+		pstmt.setInt(4,board.getStore_no());
 		int rowCount = pstmt.executeUpdate();
 		pstmt.close();
 		ConnectionFactory.releaseConnection(con);
 		return rowCount;
 	}
+	
 	public Board selectByNo(int store_no) throws Exception {
-		Board board=null;
-		Connection con=ConnectionFactory.getConnection();
-		PreparedStatement pstmt=con.prepareStatement(BoardSQL.BOARD_SELECT_STORE_NO);
-		pstmt.setInt(1, store_no);
-		ResultSet rs=pstmt.executeQuery();
+		Board board = null;
+		Connection con = ConnectionFactory.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(BoardSQL.BOARD_SELECT_BY_NO);
+		pstmt.setInt(1,store_no);
+		ResultSet rs = pstmt.executeQuery();
 		if (rs.next()) {
-			int board_no=rs.getInt("board_no");
-	        Date board_date=rs.getDate("board_date");
-	        String board_content=rs.getString("board_content");
-	        double board_rating=rs.getDouble("board_rating");
-					        
+			board = new Board(rs.getInt("board_no"),
+					          rs.getDate("board_date"),
+					          rs.getString("board_content"),
+					          rs.getInt("board_rating"),
+					          rs.getInt("store_no")
+					 	 	 );
 		}
 		rs.close();
 		pstmt.close();
 		ConnectionFactory.releaseConnection(con);
-	    return board;
+		return board;
+		
 	}
 	public List<Board> selectAll() throws Exception {
-		ArrayList<Board> boardList= new ArrayList<Board>();
-		Connection con=ConnectionFactory.getConnection();
-		PreparedStatement pstmt=con.prepareStatement(BoardSQL.BOARD_SELECT_ALL);
+		ArrayList<Board> boardList = new ArrayList<Board>();
+		Connection con = ConnectionFactory.getConnection();
+		PreparedStatement pstmt = con.prepareStatement(BoardSQL.BOARD_SELECT_ALL);
 		ResultSet rs = pstmt.executeQuery();
 		while (rs.next()) {
-			int board_no=rs.getInt("board_no");
-			Date board_date=rs.getDate("board_no");
-	        String board_content=rs.getString("board_content");
-	        double board_rating=rs.getDouble("board_rating");
-	        int store_no=rs.getInt("store_no");
-						
+			boardList.add(new Board(rs.getInt("board_no"),
+					  rs.getDate("board_date"),
+			          rs.getString("board_content"),
+			          rs.getInt("board_rating"),
+			          rs.getInt("store_no")
+			          ));
+					
 		}
 		rs.close();
 		pstmt.close();
 		ConnectionFactory.releaseConnection(con);
 		return boardList;
-		
+				
 	}
 	public int updateByNo(Board board) throws Exception {
 		Connection con = ConnectionFactory.getConnection();
-		PreparedStatement pstmt = con.prepareStatement(BoardSQL.BOARD_UPDATE);
-		pstmt.setInt(1, board.getStore_no());
-		pstmt.setInt(2, board.getBoard_rating());
-		pstmt.setString(3, board.getBoard_content());
-		pstmt.setInt(4, board.getBoard_no());
-		int rowCount=pstmt.executeUpdate();
+		PreparedStatement pstmt = con.prepareStatement(BoardSQL.BOARD_UPDATE_BY_NO);
+		pstmt.setString(1,board.getBoard_content());
+		pstmt.setInt(2,board.getBoard_rating());
+		pstmt.setInt(3,board.getStore_no());
+		int rowCount = pstmt.executeUpdate();
 		pstmt.close();
 		ConnectionFactory.releaseConnection(con);
 		return rowCount;
-		
-		
+
 	}
 	public int deleteByNo(int board_no) throws Exception {
 		Connection con = ConnectionFactory.getConnection();
-		PreparedStatement pstmt = con.prepareStatement(BoardSQL.BOARD_DELETE);
+		PreparedStatement pstmt = con.prepareStatement(BoardSQL.BOARD_DELETE_BY_NO);
 		pstmt.setInt(1,board_no);
-		int rowCount=pstmt.executeUpdate();
+		int rowCount = pstmt.executeUpdate();
 		pstmt.close();
 		ConnectionFactory.releaseConnection(con);
 		return rowCount;
