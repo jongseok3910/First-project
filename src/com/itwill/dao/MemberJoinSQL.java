f7904db2e345d37db8634506b550979aa51e7b06
qkrwhddms <qkrwhddms01@gmail.com>
2021-02-03 18:56:28 +0900

    .

--- src/com/itwill/dao/MemberJoinSQL.java
+++
@@ -1,7 +0,0 @@
-package com.itwill.dao;
-
-public class MemberJoinSQL {
-	public static final String MemberJoin_insert=
-			"insert into memberjoin(member_id, member_password, member_name, member_phone, member_address, member_joindate)"
-			+ "values(?,?,?,?,?,?)";
-}
