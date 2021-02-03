d2493bc803a8a3a07d1ca1590236c23ec20aad52
HyeJo <zhzh2032@naver.com>
2021-02-03 22:04:10 +0900

    ..

---
+++ src/com/itwill/ui/basketPanel.java
@@ -0,0 +1,106 @@
+package com.itwill.ui;
+
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Font;
+
+public class basketPanel extends JPanel {
+	private JTable basketTable;
+	private JTextField totalTF;
+	private JTextField textField;
+
+	/**
+	 * Create the panel.
+	 */
+	public basketPanel() {
+		setBackground(new Color(204, 255, 204));
+		setLayout(null);
+		
+		basketTable = new JTable();
+		basketTable.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"\uC74C\uC2DD\uBA85", "\uC218\uB7C9"
+			}
+		));
+		basketTable.setBounds(36, 69, 340, 210);
+		add(basketTable);
+		
+		JLabel totalLb = new JLabel("총금액");
+		totalLb.setFont(new Font("함초롬돋움", Font.PLAIN, 12));
+		totalLb.setHorizontalAlignment(SwingConstants.CENTER);
+		totalLb.setBounds(36, 337, 62, 21);
+		add(totalLb);
+		
+		totalTF = new JTextField();
+		totalTF.setEditable(false);
+		totalTF.setBounds(118, 337, 205, 21);
+		add(totalTF);
+		totalTF.setColumns(10);
+		
+		JLabel wonLb = new JLabel("원");
+		wonLb.setFont(new Font("함초롬돋움", Font.PLAIN, 12));
+		wonLb.setHorizontalAlignment(SwingConstants.CENTER);
+		wonLb.setBounds(335, 340, 31, 15);
+		add(wonLb);
+		
+		JLabel requestLb = new JLabel("요청사항");
+		requestLb.setFont(new Font("함초롬돋움", Font.PLAIN, 12));
+		requestLb.setHorizontalAlignment(SwingConstants.CENTER);
+		requestLb.setBounds(35, 412, 57, 15);
+		add(requestLb);
+		
+		textField = new JTextField();
+		textField.setBounds(118, 409, 251, 21);
+		add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("결제수단");
+		lblNewLabel.setFont(new Font("함초롬돋움", Font.PLAIN, 12));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(40, 488, 57, 15);
+		add(lblNewLabel);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(new Font("함초롬돋움", Font.PLAIN, 12));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"카드결제", "현장결제"}));
+		comboBox.setBounds(118, 480, 251, 23);
+		add(comboBox);
+		
+		JButton paymentBtn = new JButton("결제하기");
+		paymentBtn.setFont(new Font("함초롬돋움", Font.PLAIN, 12));
+		paymentBtn.setBounds(35, 556, 132, 23);
+		add(paymentBtn);
+		
+		JButton cancleBtn = new JButton("취소");
+		cancleBtn.setFont(new Font("함초롬돋움", Font.PLAIN, 12));
+		cancleBtn.setBounds(237, 556, 132, 23);
+		add(cancleBtn);
+		
+		JLabel listLb = new JLabel("장바구니 목록");
+		listLb.setFont(new Font("함초롬돋움", Font.BOLD, 13));
+		listLb.setHorizontalAlignment(SwingConstants.CENTER);
+		listLb.setBounds(24, 24, 368, 15);
+		add(listLb);
+
+	}
+}
---
+++ src/com/itwill/ui/foodSelectPanel.java
@@ -0,0 +1,184 @@
+package com.itwill.ui;
+
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JTabbedPane;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+
+public class foodSelectPanel extends JPanel {
+	private JTable sandwichTable;
+	private JTable labTable;
+	private JTable saladTable;
+	private JTable sidemenuTable;
+	private JTable cookieTable;
+	private JTable drinkTable;
+	private JButton basketBtn;
+	private JComboBox quantityCB;
+
+	/**
+	 * Create the panel.
+	 */
+	public foodSelectPanel() {
+		setBackground(new Color(204, 255, 204));
+		setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(19, 107, 374, 436);
+		add(tabbedPane);
+		
+		JPanel sandwichPanel = new JPanel();
+		tabbedPane.addTab("샌드위치", null, sandwichPanel, null);
+		sandwichPanel.setLayout(new BorderLayout(0, 0));
+		
+		sandwichTable = new JTable();
+		sandwichTable.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"\uC74C\uC2DD\uBA85", "\uAC00\uACA9"
+			}
+		));
+		sandwichPanel.add(sandwichTable, BorderLayout.CENTER);
+		
+		JPanel labPanel = new JPanel();
+		tabbedPane.addTab("랩", null, labPanel, null);
+		labPanel.setLayout(new BorderLayout(0, 0));
+		
+		labTable = new JTable();
+		labTable.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"\uC74C\uC2DD\uBA85", "\uAC00\uACA9"
+			}
+		));
+		labPanel.add(labTable, BorderLayout.CENTER);
+		
+		JPanel saladPanel = new JPanel();
+		tabbedPane.addTab("샐러드", null, saladPanel, null);
+		saladPanel.setLayout(new BorderLayout(0, 0));
+		
+		saladTable = new JTable();
+		saladPanel.add(saladTable, BorderLayout.CENTER);
+		
+		JPanel sidePanel = new JPanel();
+		tabbedPane.addTab("사이드메뉴", null, sidePanel, null);
+		sidePanel.setLayout(new BorderLayout(0, 0));
+		
+		sidemenuTable = new JTable();
+		sidemenuTable.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"\uC74C\uC2DD\uBA85", "\uAC00\uACA9"
+			}
+		));
+		sidePanel.add(sidemenuTable);
+		
+		JPanel cookiePanel = new JPanel();
+		tabbedPane.addTab("쿠키", null, cookiePanel, null);
+		cookiePanel.setLayout(new BorderLayout(0, 0));
+		
+		cookieTable = new JTable();
+		cookieTable.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"\uC74C\uC2DD\uBA85", "\uAC00\uACA9"
+			}
+		));
+		cookiePanel.add(cookieTable, BorderLayout.CENTER);
+		
+		JPanel drinkPanel = new JPanel();
+		tabbedPane.addTab("음료", null, drinkPanel, null);
+		drinkPanel.setLayout(new BorderLayout(0, 0));
+		
+		drinkTable = new JTable();
+		drinkTable.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"\uC74C\uC2DD\uBA85", "\uAC00\uACA9"
+			}
+		));
+		drinkPanel.add(drinkTable, BorderLayout.CENTER);
+		
+		JComboBox storeCheckCB = new JComboBox();
+		storeCheckCB.setBounds(105, 44, 277, 23);
+		add(storeCheckCB);
+		
+		basketBtn = new JButton("장바구니");
+		basketBtn.setFont(new Font("함초롬돋움", Font.PLAIN, 12));
+		basketBtn.setBounds(286, 571, 97, 23);
+		add(basketBtn);
+		
+		JLabel storeLb = new JLabel("지점선택");
+		storeLb.setFont(new Font("함초롬돋움", Font.BOLD, 14));
+		storeLb.setBounds(34, 48, 57, 15);
+		add(storeLb);
+		
+		quantityCB = new JComboBox();
+		quantityCB.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}));
+		quantityCB.setBounds(76, 571, 70, 23);
+		add(quantityCB);
+		
+		JLabel lblNewLabel = new JLabel("수량");
+		lblNewLabel.setFont(new Font("함초롬돋움", Font.PLAIN, 12));
+		lblNewLabel.setBounds(34, 575, 57, 15);
+		add(lblNewLabel);
+
+	}
+}
