03bb81f914479076c1c9a2e8f9ba1094f1fa8bb4
jongseok3910 <jongseok3910@gmail.com>
2021-02-03 11:54:27 +0900

    수정

--- src/com/itwill/dao/BoardSQL.java
+++ src/com/itwill/dao/BoardSQL.java
@@ -1,14 +1,14 @@
 package com.itwill.dao;
 public class BoardSQL {
 public static final String BOARD_INSERT=
-"insert into board(board_seq1.nextval, board_date, board_content, board_rating, store_no) values(?,sysdate,?,?,?)";
+"insert into board(board_no, board_date, board_content, board_rating, store_no) values(board_seq1.nextval,sysdate,?,?,?)";
 public static final String BOARD_SELECT_BY_NO=
-"select board_no, board_date, board_content, board_rating, from board where store_no=?";
+"select board_seq1.nextval, board_date, board_content, board_rating, from board where store_no=?";
 public static final String BOARD_SELECT_ALL=
 "select * from board";
 public static final String BOARD_UPDATE_BY_NO=
-"update board set board_content=?, board_rating=?, store_no=? where board_no=?";
+"update board set board_content=?, board_rating=?, store_no=? where board_seq1.nextval=?";
 public static final String BOARD_DELETE_BY_NO=
-"delete board where board_no=?";
+"delete board where board_seq1.nextval=?";
 
 }
