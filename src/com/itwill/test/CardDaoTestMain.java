35ae06fa495503fa45c656995d71984013e30e51
academyitwill <academyitwill@gmail.com>
2021-02-04 16:44:11 +0900

    .

--- src/com/itwill/test/CardDaoTestMain.java
+++ src/com/itwill/test/CardDaoTestMain.java
@@ -9,7 +9,7 @@ public class CardDaoTestMain {
 
 	public static void main(String[] args) throws Exception {
 		CardDao cardDao = new CardDao();
-		Card card = new Card("0000-0000-0000-0000", "21/11", 123, 999999);
+//		Card card = new Card("0000-0000-0000-0000", "21/11", 123, 999999);
 
 //		create test
 //		System.out.println("insert : " + cardDao.create(card));
