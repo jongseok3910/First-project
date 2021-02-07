35c80abaaf934c1731f10fb56d66e732e1e1130e
qkrwhddms <qkrwhddms01@gmail.com>
2021-02-08 00:27:06 +0900

    .

--- src/com/itwill/test/CardDaoTestMain.java
+++ src/com/itwill/test/CardDaoTestMain.java
@@ -1,22 +1,26 @@
 package com.itwill.test;
-/*
- * test 완료
- */
+
 import com.itwill.dao.CardDao;
 import com.itwill.vo.Card;
+
 
 public class CardDaoTestMain {
 
 	public static void main(String[] args) throws Exception {
+		Card card = new Card("2323-3454-6666-3345","54/34", 777,878787,"202102038");
 		CardDao cardDao = new CardDao();
-		Card card = new Card("0000-0000-0000-0000", "21/11", 123, 999999);
-
-//		create test
+		
+//		create method test
 //		System.out.println("insert : " + cardDao.create(card));
 		
-//		delete test
-//		System.out.println("delete : " + cardDao.deleteByNo("0000-0000-0000-0000"));
+//		updateByMemberNo method test
+//		System.out.println("updateByMemberNo : " + cardDao.updateByNo(new Card("4444-4444-4444-4444","54/23",898,567567,"202102031")));
 		
+//		deleteByCardNo method test
+//		System.out.println("deleteByCardNo : " + cardDao.deleteByNo("1465-1111-2222-6337"));
+		
+//		selectByMemberNo method test
+//		System.out.println("selectByMemberNo : " + cardDao.selectByMemberNo("202102034"));
 	}
 
 }
