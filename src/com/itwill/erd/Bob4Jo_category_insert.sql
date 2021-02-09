e4a2c8165f44f28620194c1c7480eda0c9bdcafa
cohic91 <cohic91@gmail.com>
2021-02-09 10:24:23 +0900

    통합 전 자료 삭제

--- src/com/itwill/erd/Bob4Jo_category_insert.sql
+++
@@ -1,8 +0,0 @@
-desc category;
---insert
-insert into category values(10,'샌드위치');
-insert into category values(20,'랩');
-insert into category values(30,'찹샐러드');
-insert into category values(40,'사이드메뉴');
-insert into category values(50,'쿠키,칩');
-insert into category values(60,'음료');
